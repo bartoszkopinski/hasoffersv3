2d0af7f03f94051d6fb3e19d44ccdb5838243752
Timo Rößner <timo.roessner@googlemail.com>
2013-09-23 11:44:38 +0200

    Use a POST request for "getConversions" because a GET request fails since we pass in too many affiliate_ids.

--- lib/hasoffersv3/report.rb
+++ lib/hasoffersv3/report.rb
@@ -4,7 +4,9 @@ module HasOffersV3
 
     class << self
       def getConversions(params = {}, &block)
-        get_request(Target, 'getConversions', params, &block)
+        # TODO: This *should* be a GET request, however, if we use that here we get: "The requested URL's length exceeds the capacity limit for this server."
+        #       because the number of affiliate_ids we pass in is too high.
+        post_request(Target, 'getConversions', params, &block)
       end
 
       def getModSummaryLogs(params = {}, &block)
